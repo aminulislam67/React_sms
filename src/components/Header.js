84204c997d44a04dc7d12c754cfdc75b99567308
aminulislam67 <98544001+aminulislam67@users.noreply.github.com>
2023-06-08 12:02:40 +0600

    1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 
 const Header = () => {
   return (
     <>
           <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">User Registration</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -19,5 +21,3 @@ const Header = () => {
 }
 
 export default Header
-
-
